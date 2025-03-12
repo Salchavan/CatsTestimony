dca648f927584bf92735f318e4381ed712bd30ce
Salvador Castellanos <salvadorcastellanosposse@gmail.com>
2025-03-12 09:23:30 -0300

    Update title case in App.js and enhance styling in Testimony.css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <header className='App-header'>
-        <h1 className='title'>Testimonios</h1>
+        <h1 className='title'>TESTIMINIOS</h1>
       </header>
       <TestimonyList />
     </>
